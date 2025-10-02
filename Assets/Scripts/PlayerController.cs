a788f86836aa9dd0bf052f337ed050243fea77b7
meldy04 <120117788+meldy04@users.noreply.github.com>
2025-10-02 12:24:07 +0100

    Visual Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
-    public float speed;
+    public float speed = 500;
 
     void OnMove(InputValue value)
     {
